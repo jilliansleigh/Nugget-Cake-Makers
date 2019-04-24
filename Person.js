f5c609af4ee981bce22bea5e7ac6bf57a7a2d436
Jillian <47332051+jilliansleigh@users.noreply.github.com>
2019-04-24 14:35:21 -0400

    Create Person.js

---
+++ Person.js
@@ -0,0 +1,28 @@
+function Person() {
+  this.pos = createVector(50, 250);
+  this.vel = createVector(1, 0);
+  this.acc = createVector(0, 0);
+  this.mass = 10;
+  this.applyForce = function(force) {
+    this.acc.add(force);
+  }
+  this.update = function() {
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    this.acc.set(0, 0);
+  }
+  this.display = function() {
+    fill(245,130,0);
+    noStroke();
+    rect(this.pos.x, this.pos.y+30, this.mass*3, this.mass*7);
+  }
+  this.edges = function() {
+    if (this.pos.y > 250) {
+      this.vel.y *= 0;
+      this.pos.y = 250;
+      
+      
+      
+    }
+  }
+}
