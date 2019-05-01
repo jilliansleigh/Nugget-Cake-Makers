4c999ae3164edc7dd4fc80ce00664ece6a21f16b
Jillian <47332051+jilliansleigh@users.noreply.github.com>
2019-05-01 13:36:47 -0400

    Update monster.js

--- monster.js
+++ monster.js
@@ -1,21 +1,42 @@
+Person.prototype.checkForCollision = function(collision) {
+    if ((collision.x >= this.x && collision.x <= (this.x + 60)) &&
+        (collision.y >= this.y && collision.y <= (this.y + 40))) {
+        collision.y = fill(90,90,90); rect(100,100,100,100);
+        this.collision++;
+    }
+};
+
 var monster = function(x, y) {
     this.x = x;
     this.y = y;
 };
 
 monster.prototype.draw = function() {
-    fill(50, 151, 78);
+    fill(50,151,78);  
     rectMode(CENTER);
-    rect(this.x, this.y, 60, 60);
+    rect(x+400,280,60,60);
+    fill(20,109,20);
+    ellipse(420+x,300,10,10);
+    ellipse(440+x,300,10,10);
+    fill(2,140,10)
+    ellipse(425+x,320,6,16); 
+    ellipse(435+x,320,6,16);
 };
 
-  monster(100);
-  monster(500);
-  monster(1000);
-  monster(1500);
-  monster(2000);
-  monster(2500);
-  monster(3000); 
-  monster(3500);
-  monster(4000);
-  monster(4500);
+var person = new Person(200, 300);
+
+var Monster = [];
+for (var i = 0; i < 40; i++) {  
+    Monster.push(new monster(i+ 400, 280));
+}
+
+
+
+var person = new Person(this.pos.x+30, this.pos.y+10);
+
+
+    for (var i = 0; i < Monster.length; i++) {
+        Monster[i].draw();
+        person.checkForStickGrab(Monster[i]);
+        Monster[i].x -= 1;
+    }
