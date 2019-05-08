7a4f38783e9b464f4e452cedae6bc610c1d60a21
Jillian <47332051+jilliansleigh@users.noreply.github.com>
2019-05-08 14:57:53 -0400

    Update Sketch.js

--- Sketch.js
+++ Sketch.js
@@ -223,7 +223,10 @@ else if(sceneNum===1){
 
 //Work in progress
 var person;
-var sceneNum=0
+var sceneNum=0;
+var centerX = floor(random(0,600));
+var centerY = floor(random(0,300));
+
 var monster = function (x) {
   fill(245,130,0);  
   rect(400+x,280,60,60);
@@ -249,36 +252,25 @@ function keyPressed(){
   }
 }
 //start screen
+
+
 function draw() {
   if(sceneNum===0){ 
     background(40);
     textSize(20);
-    noStroke();
-    fill(255, 255, 255);
-    ellipse(200,200,10,10);
-    ellipse(300,100,6,6);
-    ellipse(100,100,7,7);
-    ellipse(200,300,10,10);
-    ellipse(150,78,8,8);
-    ellipse(400,155,9,9);
-    ellipse(10,210,7,7);
-    ellipse(489,327,9,9);
-    ellipse(360,323,10,10);
-    ellipse(378,46,5,5);
-    ellipse(23,70,8,8);
-    ellipse(90,280,5,5);
-    ellipse(120,230,7,7);
-    ellipse(400,270,9,9);
-    ellipse(370,100,9,9);
-    ellipse(500,200,9,9);
-    ellipse(600,209,10,10); 
-    ellipse(570,20,7,7);
-    ellipse(550,200,6,6);
-    ellipse(520,150,8,8);
-    ellipse(468,300,9,9);
-    ellipse(70,309,9,9);
-    ellipse(130,40,10,10);
-    ellipse(170,170,9,9); 
+   noStroke();
+var drawStar = function(){
+
+var StarLength = 9;
+var StarHeight = 9;
+fill(255,255,255);
+
+noStroke();
+
+ellipse(floor(random(0,600)), floor(random(0,300)), StarLength, StarHeight);
+};
+drawStar();
+    
     fill(0,255,127);
     text("THE EQUINOX DELIVERY",100,100);
     fill(255,20,147);
