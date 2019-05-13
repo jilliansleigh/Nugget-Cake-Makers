e4202738bc41559cde98edd5d18da0a84748a741
Jillian <47332051+jilliansleigh@users.noreply.github.com>
2019-05-13 12:51:37 -0400

    Update Sketch.js

--- Sketch.js
+++ Sketch.js
@@ -1,14 +1,14 @@
 var person;
 var sceneNum=0
 var monster = function (x) {
-  fill(50,151,78);  
-  rect(400+x,280,60,60);
-  fill(20,109,20);
-  ellipse(420+x,300,10,10);
-  ellipse(440+x,300,10,10);
-  fill(2,140,10)
-  ellipse(425+x,320,6,16);
-  ellipse(435+x,320,6,16);
+  fill(200,109,20);  
+  rect(400+x,280,45,45);
+  fill(139,69,19);
+  ellipse(415+x,300,8,8);
+  ellipse(430+x,300,8,8);
+  fill(129,69,19)
+  ellipse(418+x,315,5,13);
+  ellipse(427+x,315,5,13);
  
 };
 
@@ -17,16 +17,7 @@ function setup() {
   createCanvas(640, 360);
   person = new Person();
 }
-/* walk??
-function walk(){
-  if (key == ' '){
-    var walk = createVector(10,0);
-  person.applyForce(walk);
-  }else if (key=='d'){ 
-    sceneNum++;
-  }
-}
-*/
+
 
 function keyPressed(){
   if (key == ' '){
@@ -39,10 +30,17 @@ function keyPressed(){
 //start screen
 function draw() {
   if(sceneNum===0){ 
-    background(40);
+    background(10, 30, 103);
     textSize(20);
     noStroke();
-    fill(255, 255, 255);
+    fill(0,255,127);
+    text("THE EQUINOX DELIVERY",100,100);
+    fill(255,20,147);
+    text("PRESS B TO BEGIN",200,200);
+    text("USE THE SPACE BAR TO JUMP",250,250);
+     fill(70,130,180);
+  	
+        fill(255,255,255);
     ellipse(200,200,10,10);
     ellipse(300,100,6,6);
     ellipse(100,100,7,7);
@@ -67,15 +65,10 @@ function draw() {
     ellipse(70,309,9,9);
     ellipse(130,40,10,10);
     ellipse(170,170,9,9); 
-    fill(0,255,127);
-    text("THE EQUINOX DELIVERY",100,100);
-    fill(255,20,147);
-    text("PRESS B TO BEGIN",200,200);
-    text("USE THE SPACE BAR TO JUMP",250,250);
-     fill(70,130,180);
-  	
+    
+    fill(177, 140, 15);//ground
     rect(0,315,1000,50);
-    fill(100,149,200);
+    fill(149, 120, 15);
     ellipse(450,329,30,15);
     ellipse(270,330,41,21); 
     ellipse(70,329,40,20);
@@ -86,13 +79,48 @@ function draw() {
     ellipse(600,340,45,25);
     ellipse(30,350,52,25);
     
+    fill(50);
+    rect(589,335,50,25);
+     fill(30,0,170);
+    text("Logo", 593,355);
+   fill(80,90,90); 
+    if(mouseIsPressed&&mouseX>589&&mouseY>335){
+      background(10, 30, 103);
+        fill(255,20,147);
+      text("Nugget Cake Makers",190,100);
+        fill(255,255,255);
+    ellipse(200,200,10,10);
+    ellipse(300,100,6,6);
+    ellipse(100,100,7,7);
+    ellipse(200,300,10,10);
+    ellipse(150,78,8,8);
+    ellipse(400,155,9,9);
+    ellipse(10,210,7,7);
+    ellipse(489,327,9,9);
+    ellipse(360,323,10,10);
+    ellipse(378,46,5,5);
+    ellipse(23,70,8,8);
+    ellipse(90,280,5,5);
+    ellipse(120,230,7,7);
+    ellipse(400,270,9,9);
+    ellipse(370,100,9,9);
+    ellipse(500,200,9,9);
+    ellipse(600,209,10,10); 
+    ellipse(570,20,7,7);
+    ellipse(550,200,6,6);
+    ellipse(520,150,8,8);
+    ellipse(468,300,9,9);
+    ellipse(70,309,9,9);
+    ellipse(130,40,10,10);
+    ellipse(170,170,9,9);       
+    }
   }
   
   //during the game screen
                          
 else if(sceneNum===1){
   noStroke();
-  background(51);
+  background(10, 30, 103);
   
   fill(255, 255, 255);//stars
     ellipse(200,200,10,10);
@@ -125,9 +153,9 @@ else if(sceneNum===1){
     ellipse(270,170,8,8);
     ellipse(60,350,52,25);//end of stars
   
-  fill(70,130,200);//ground
+  fill(177, 140, 15);//ground
     rect(0,315,1000,50);
-    fill(100,149,200);
+    fill(149, 120, 15);
     ellipse(450,329,30,15);
     ellipse(270,330,41,21); 
     ellipse(70,329,40,20);
@@ -139,7 +167,7 @@ else if(sceneNum===1){
     ellipse(30,350,52,25);//end of ground
   translate(-person.pos.x,0);
   //gravity
-  var gravity = createVector(0,0.122);
+  var gravity = createVector(0,0.11);
   person.applyForce(gravity);
   if(mouseIsPressed){
   
@@ -171,7 +199,7 @@ else if(sceneNum===1){
 }else{
   
   //end screen-> lose
-  background(40);
+  background(10, 30, 103);
   noStroke();
   fill(255, 255, 255);
     ellipse(200,200,10,10);
@@ -202,10 +230,10 @@ else if(sceneNum===1){
     ellipse(50,260,9,9);
     ellipse(260,230,9,9);
     ellipse(270,170,8,8);
-    fill(70,130,180);
+    fill(177, 140, 15);
     
   rect(0,315,1000,50);
-    fill(100,149,200);
+    fill(149, 120, 15);
     ellipse(450,329,30,15);
     ellipse(270,330,41,21); 
     ellipse(70,329,40,20);
@@ -218,7 +246,7 @@ else if(sceneNum===1){
      fill(255,7,92);
     text("YOU LOSE!",250,200);
 	}
-} 
+}  
  
 
 //Work in progress
