4b7b8ecda225f0c074ee022e86c97f4de6557e56
Sevdazay <47332055+Sevdazay@users.noreply.github.com>
2019-05-23 13:32:24 -0400

    Update monster.js

--- monster.js
+++ monster.js
@@ -1,31 +1,15 @@
-function Monster() {
-  this.x=0;
-  this.y=0;
+function Monster(x,y) {
+  this.x=x;
+  this.y=y;
   
-  this.show();{
+  this.show=function(){
     fill(200,109,20);  
-  rect(400+this.x,280,45,45);
+  rect(400+this.x,280-this.y,45,45);
   fill(139,69,19);
-  ellipse(415+this.x,300,8,8);
-  ellipse(430+this.x,300,8,8);
+  ellipse(415+this.x,300-this.y,8,8);
+  ellipse(430+this.x,300-this.y,8,8);
   fill(129,69,19)
-  ellipse(418+this.x,315,5,13);
-  ellipse(427+this.x,315,5,13);
+  ellipse(418+this.x,315-this.y,5,13);
+  ellipse(427+this.x,315-this.y,5,13);
   }
-  
-  this.update();{
-    
-  }
-}
-keyUpHandler();{
-function collisionDetection() {
-    for(var c=0; c<monsterColumnCount; c++) {
-        for(var r=0; r<monsterRowCount; r++) {
-            var b = Monster[c][r];
-            if(x > b.x && x < b.x+monsterWidth && y > b.y && y < b.y+monsterHeight) {
-                dy = -dy;
-            }
-        }
-    }
-} 
 }
