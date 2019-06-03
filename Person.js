14b69d2cd3649007c46966491a62f74794c22f6a
Jillian <47332051+jilliansleigh@users.noreply.github.com>
2019-06-03 12:56:21 -0400

    Update Person.js

--- Person.js
+++ Person.js
@@ -12,19 +12,36 @@ function Person() {
     this.acc.set(0, 0);
   }
   this.display = function() {
-    fill(245,130,0);
+    fill(0, 51, 102);
     noStroke();
     //left anntena
-    rect(this.pos.x+20, this.pos.y-5,this.mass*1, this.mass*1);
+    rect(this.pos.x+20, this.pos.y-2,this.mass*1, this.mass*1,30);
     //right anntena
-     rect(this.pos.x+55, this.pos.y-5,this.mass*1, this.mass*1);
+     rect(this.pos.x+55, this.pos.y-2,this.mass*1, this.mass*1,30);
+    fill(139,129,169);
+    
     //body
     rect(this.pos.x+30, this.pos.y+10, this.mass*3, this.mass*7);
+    fill(204, 102, 153);
+    rect(this.pos.x+30, this.pos.y+40, this.mass*2.96, this.mass*4);
     //left eye
-    fill(39,178,309);
-     rect(this.pos.x+47, this.pos.y+25,this.mass*1, this.mass*1);
+    fill(0,0,0);
+     rect(this.pos.x+47, this.pos.y+23,this.mass*1, this.mass*1,30);
     //right eye
-       rect(this.pos.x+32, this.pos.y+25,this.mass*1, this.mass*1);
+    fill(0,0,0);
+       rect(this.pos.x+32, this.pos.y+23,this.mass*1, this.mass*1,30);
+   
+      
+    fill(0, 51, 102);
+    rect(this.pos.x+29, this.pos.y+5,this.mass*3.2, this.mass*1);
+    fill(128, 0, 0);
+//bow    
+  rect(this.pos.x+32, this.pos.y+2,this.mass*1, this.mass*1,2);
+  rect(this.pos.x+45, this.pos.y+2,this.mass*1, this.mass*1,2);
+      rect(this.pos.x+40, this.pos.y+4,this.mass*1, this.mass*0.5);
+    fill(0,0,0);
+    rect(this.pos.x+40, this.pos.y+34,this.mass*1, this.mass*0.4);
+    
   }
   this.edges = function() {
     if (this.pos.y > 250) {
@@ -36,6 +53,5 @@ function Person() {
     }
   }
 }
-
 
 
