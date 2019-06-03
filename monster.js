d56c39dc34d8262a8a71f8ed5939601d23cd81fe
Jillian <47332051+jilliansleigh@users.noreply.github.com>
2019-06-03 12:56:58 -0400

    Update monster.js

--- monster.js
+++ monster.js
@@ -3,13 +3,23 @@ function Monster(x,y) {
   this.y=y;
   
   this.show=function(){
-    fill(200,109,20);  
-  rect(400+this.x,280-this.y,45,45);
-  fill(139,69,19);
-  ellipse(415+this.x,300-this.y,8,8);
-  ellipse(430+this.x,300-this.y,8,8);
-  fill(129,69,19)
-  ellipse(418+this.x,315-this.y,5,13);
-  ellipse(427+this.x,315-this.y,5,13);
+// body
+    fill(246, 220, 84);  
+  rect(400+this.x,280-this.y,45,45,7);
+    
+// face
+  fill(0,0,0);
+  rect(405+this.x,290-this.y, 33,2);
+  rect(415+this.x,290-this.y,9,9,3);
+  rect(430+this.x,290-this.y,9,9,3);
+  ellipse(425+this.x,315-this.y,13,13);
+    
+    // hat
+       fill(28, 72, 166);
+   ellipse(442+this.x,280-this.y, 43,12);
+      fill(28, 72, 220);
+  rect(399+this.x,277-this.y, 46,10);
+  ellipse(423+this.x,277-this.y, 47,10);
+   
   }
 }
